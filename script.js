ac11106afbaf2ba736672aa029c911c76ae11cad
Py-Ru <ashvinmaruti@gmail.com>
2022-08-24 02:51:15 +0100

    from early days

---
+++ script.js
@@ -0,0 +1,73 @@
+/* 
+ GAME FUNCTIONS:
+ - Player must guess a number between a min and a max
+ - Player gets a certain amount of guesses
+ - Notify player of guesses remaining
+ - Notify the player of the correc answer if player loses
+ - Lets player choose to play again
+ */
+
+let min = 1,
+  max = 10,
+  winningNum = getRandomNum(min, max),
+  guessesLeft = 3;
+
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener("click", function () {
+  let guess = parseInt(guessInput.value);
+
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  if (guess === winningNum) {
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  } else {
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      gameOver(
+        false,
+        `GAME OVER, YOU LOST. The correct number was ${winningNum}`
+      );
+    } else {
+      guessInput.value = "";
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, "red");
+    }
+  }
+});
+
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  guessInput.disabled = true;
+  guessInput.style.borderColor = color;
+  setMessage(msg, color);
+
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+}
+
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
